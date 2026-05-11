bc355ebbb6d1dc67d8c5f945bef74eb3204c8c51
simmipawar03 <simmipawar03@gmail.com>
2026-05-11 15:06:37 +0530

    Delete Hotel-Rating-Service-main/Hotel-Rating-Service-main directory

--- Hotel-Rating-Service-main/Hotel-Rating-Service-main/HotelService/src/main/java/com/luwd/hotel/service/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luwd.hotel.service.repositories;
-
-import com.luwd.hotel.service.entites.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
--- Hotel-Rating-Service-main/Hotel-Rating-Service-main/UserService/src/main/java/com/luwd/user/service/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luwd.user.service.repositories;
-
-import com.luwd.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-
-}
